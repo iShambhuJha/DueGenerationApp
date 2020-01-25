1a5c83352fd0f3f9a63f21627a7d3c87c8af7f18
jha-sn <shambhu.jha014@gmail.com>
2020-01-25 23:58:56 +0530

    server changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/DueGeneration'));
+app.use(express.static(__dirname + '/dist/DueGenerationApp'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/DueGeneration/index.html'));
+res.sendFile(path.join(__dirname+'/dist/DueGenerationApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
